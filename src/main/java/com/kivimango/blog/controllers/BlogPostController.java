7300a34d238ecbb8b53832c8993f1845058f09e7
kivimango <balazsviktor23@gmail.com>
2017-11-04 23:04:34 +0100

    Added BlogPostController

---
+++ src/main/java/com/kivimango/blog/controllers/BlogPostController.java
@@ -0,0 +1,56 @@
+package com.kivimango.blog.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import com.kivimango.blog.exception.BlogPostNotFoundException;
+import com.kivimango.blog.services.BlogPostService;
+import com.kivimango.blog.services.TagService;
+
+/** 
+ * Displaying a full post to the user.
+ * Posts can be queried by their slug.
+ * 
+ * @author kivimango
+ * @since 0.1
+ * @version 0.1
+ */
+
+@Controller
+public class BlogPostController {
+	
+	@Value("${blog.title}")
+	private String title;
+	
+	@Value("${blog.description}")
+	private String description;
+	
+	private static final String DETAIL_PAGE = "blogPost";
+	
+	private BlogPostService service;
+	
+	private TagService tags;
+	
+	@GetMapping("poszt/{slug}")
+	public String readBlogPost(@PathVariable(name="slug") String slug, Model model) throws BlogPostNotFoundException {
+		model.addAttribute("blogTitle", title);
+		model.addAttribute("title", title + " - " + description);
+		model.addAttribute("post", service.getPostBySlug(slug));
+		model.addAttribute("tags", tags.getFirstTenTags());
+		return DETAIL_PAGE;
+	}
+	
+	@Autowired
+	public void setService(BlogPostService service) {
+		this.service = service;
+	}
+	
+	@Autowired
+	public void setTagService(TagService service) {
+		this.tags = service;
+	}
+
+}
