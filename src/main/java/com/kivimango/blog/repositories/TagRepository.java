6f263c3b32c529fb3be0d791e6a49e628f5357ac
kivimango <balazsviktor23@gmail.com>
2017-11-04 23:06:01 +0100

    Added TagRepository

---
+++ src/main/java/com/kivimango/blog/repositories/TagRepository.java
@@ -0,0 +1,18 @@
+package com.kivimango.blog.repositories;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.kivimango.blog.domain.entity.Tag;
+
+/**
+ * @author kivimango
+ * @since 0.1
+ * @version 0.1
+ */
+
+@Repository
+public interface TagRepository extends JpaRepository<Tag, Integer> {
+
+	public List<Tag> findTop10ByOrderByTagAsc();
+}
