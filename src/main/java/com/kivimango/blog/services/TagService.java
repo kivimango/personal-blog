16e9227b32418f8b2d8a02c8d4f7cbdae57975df
kivimango <balazsviktor23@gmail.com>
2017-11-04 23:07:04 +0100

    Added TagService

---
+++ src/main/java/com/kivimango/blog/services/TagService.java
@@ -0,0 +1,16 @@
+package com.kivimango.blog.services;
+
+import java.util.List;
+import com.kivimango.blog.domain.entity.Tag;
+
+/** 
+ * @author kivimango
+ * @since 0.1
+ * @version 0.1
+ */
+
+public interface TagService {
+	
+	public List<Tag> getFirstTenTags();
+
+}
