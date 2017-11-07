1b8b1f115e0387e2a99f923c39fee6266c373af4
kivimango <balazsviktor23@gmail.com>
2017-11-07 17:27:08 +0100

    Added AdminRepository

---
+++ src/main/java/com/kivimango/blog/repositories/AdminRepository.java
@@ -0,0 +1,18 @@
+package com.kivimango.blog.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.kivimango.blog.domain.entity.Admin;
+
+/**
+ * @author kivimango
+ * @since 0.1
+ * @version 0.1
+ */
+
+@Repository
+public interface AdminRepository extends JpaRepository<Admin, Short> {
+	
+	Admin findAdminByUsername(String username);
+
+}
