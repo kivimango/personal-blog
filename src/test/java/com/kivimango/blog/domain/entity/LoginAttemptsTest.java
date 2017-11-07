9ab9adeae00c20508274f2960be859d18356c156
kivimango <balazsviktor23@gmail.com>
2017-11-07 17:26:16 +0100

    Added LoginAttempt  entity unit test

---
+++ src/test/java/com/kivimango/blog/domain/entity/LoginAttemptsTest.java
@@ -0,0 +1,31 @@
+package com.kivimango.blog.domain.entity;
+
+import static org.junit.Assert.assertEquals;
+
+import java.sql.Timestamp;
+import org.junit.Before;
+import org.junit.Test;
+
+public class LoginAttemptsTest {
+	
+	Integer id;
+	String ipAdress;
+	Timestamp timestamp;
+	LoginAttempt loginAttempt;
+	
+	@Before
+	public void when() {
+		id = 42;
+		ipAdress = "127.0.0.1";
+		timestamp = new Timestamp(System.currentTimeMillis());
+		loginAttempt = new LoginAttempt(ipAdress, timestamp);
+		loginAttempt.setId(id);
+	}
+	
+	@Test
+	public void testLoginAttemptEntityShouldEqual() {
+		assertEquals(id,loginAttempt.getId());
+		assertEquals(ipAdress, loginAttempt.getIpAdress());
+		assertEquals(timestamp, loginAttempt.getAttemptDate());
+	}
+}
