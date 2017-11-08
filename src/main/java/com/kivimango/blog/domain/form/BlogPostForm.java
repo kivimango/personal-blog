d7f02c4e707ec87828142dae38347b20302f3898
kivimango <balazsviktor23@gmail.com>
2017-11-08 20:55:36 +0100

    Added BlogPostForm

---
+++ src/main/java/com/kivimango/blog/domain/form/BlogPostForm.java
@@ -0,0 +1,55 @@
+package com.kivimango.blog.domain.form;
+
+import javax.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
+
+public class BlogPostForm {
+	
+	@NotNull
+	@Length(min=3, max=30)
+	private String author;
+
+	@NotNull
+	@Length(min=3, max=150)
+	private String title;
+	
+	@NotNull
+	@Length(min=3, max=10000)
+	private String content;
+	
+	@Length(max=500)
+	private String tags;
+	
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getTags() {
+		return tags;
+	}
+
+	public void setTags(String tags) {
+		this.tags = tags;
+	}
+
+}
