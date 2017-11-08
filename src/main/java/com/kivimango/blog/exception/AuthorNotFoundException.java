714ee4653c0ef1fe80024b36745077303ed5a5f8
kivimango <balazsviktor23@gmail.com>
2017-11-08 20:55:58 +0100

    Added AuthorNotFoundException

---
+++ src/main/java/com/kivimango/blog/exception/AuthorNotFoundException.java
@@ -0,0 +1,11 @@
+package com.kivimango.blog.exception;
+
+public class AuthorNotFoundException extends Exception {
+
+	private static final long serialVersionUID = -3782874283805145753L;
+
+	public AuthorNotFoundException(String arg0) {
+		super(arg0);
+	}
+	
+}
