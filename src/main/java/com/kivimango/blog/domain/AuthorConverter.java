269dde24697e13a33cd010bac780fe3f55a11e11
kivimango <balazsviktor23@gmail.com>
2017-11-09 19:59:00 +0100

    Overloaded convert() with lists

--- src/main/java/com/kivimango/blog/domain/AuthorConverter.java
+++ src/main/java/com/kivimango/blog/domain/AuthorConverter.java
@@ -1,5 +1,8 @@
 package com.kivimango.blog.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.kivimango.blog.domain.entity.Author;
 
 public class AuthorConverter {
@@ -11,6 +14,14 @@ public class AuthorConverter {
 		converted.setFbProfile(author.getFbProfile());
 		converted.setTwitterProfile(author.getTwitterProfile());
 		converted.setLinkedinProfile(author.getLinkedinProfile());
+		return converted;
+	}
+
+	public Iterable<AuthorView> convert(Iterable<Author> list) {
+		List<AuthorView> converted = new ArrayList<AuthorView>();
+		for(Author a : list) {
+			converted.add(convert(a));
+		}
 		return converted;
 	}
 
