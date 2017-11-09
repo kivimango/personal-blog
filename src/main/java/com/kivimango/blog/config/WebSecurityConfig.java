738a1af6b020f9ea79d9fbf9ce7931a301e0a3b8
kivimango <balazsviktor23@gmail.com>
2017-11-09 20:08:21 +0100

    Added web security config

---
+++ src/main/java/com/kivimango/blog/config/WebSecurityConfig.java
@@ -0,0 +1,57 @@
+package com.kivimango.blog.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/**
+ * @author kivimango
+ * @since 0.1
+ * @version 0.1
+ */
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled=true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private UserDetailsService service;
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// Don't change the strength parameter in the encoder's constructor, otherwise it will break the login
+		auth.userDetailsService(service).passwordEncoder(new BCryptPasswordEncoder(12));
+	}
+
+	@Profile("dev")
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		//http.csrf().disable();
+		//http.headers().frameOptions().disable();
+		http
+			.authorizeRequests()
+				.antMatchers("/dashboard/**").hasRole("ADMIN").anyRequest().permitAll()
+				.antMatchers("/resources/**").permitAll()
+				.antMatchers("/**").permitAll()
+				.and()
+			.formLogin()
+				.loginPage("/dashboard/login")
+				.loginProcessingUrl("/security_check")
+				.defaultSuccessUrl("/dashboard")
+				.permitAll()
+			.and()
+				.logout()
+				.logoutUrl("dashboard/kilep")
+				.logoutSuccessUrl("/dashboard/login?logout")
+				.permitAll();
+	}
+
+}
