bf622c49b79b6695f2b40799de3ce38cd027ef81
kivimango <balazsviktor23@gmail.com>
2017-11-15 01:38:36 +0100

    Refactor, convert() takes Admin as a parameter now

--- src/main/java/com/kivimango/blog/domain/AuthorConverter.java
+++ src/main/java/com/kivimango/blog/domain/AuthorConverter.java
@@ -1,13 +1,10 @@
 package com.kivimango.blog.domain;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import com.kivimango.blog.domain.entity.Author;
+import com.kivimango.blog.domain.entity.Admin;
 
 public class AuthorConverter {
 	
-	public AuthorView convert(final Author author) {
+	public AuthorView convert(final Admin author) {
 		AuthorView converted = new AuthorView();
 		converted.setName(author.getName());
 		converted.setAvatar(author.getAvatar());
@@ -16,13 +13,4 @@ public class AuthorConverter {
 		converted.setLinkedinProfile(author.getLinkedinProfile());
 		return converted;
 	}
-
-	public Iterable<AuthorView> convert(Iterable<Author> list) {
-		List<AuthorView> converted = new ArrayList<AuthorView>();
-		for(Author a : list) {
-			converted.add(convert(a));
-		}
-		return converted;
-	}
-
 }
