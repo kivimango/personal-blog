8685eac4b95c88eb6ac7dddd64159faae6e2c426
kivimango <balazsviktor23@gmail.com>
2017-11-15 01:42:34 +0100

    Dropped multiple authors feature, Admin is considered now as the author (hence the name personal-blog)

--- src/main/java/com/kivimango/blog/domain/entity/Admin.java
+++ src/main/java/com/kivimango/blog/domain/entity/Admin.java
@@ -1,10 +1,13 @@
 package com.kivimango.blog.domain.entity;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -22,6 +25,24 @@ public class Admin {
     private String password;
     
     private Date lastLogin;
+    
+    @Column(nullable=false,unique=true)
+	private String name;
+	
+	@OneToMany(mappedBy="author")
+	private List<BlogPost> posts = new ArrayList<BlogPost>(0);
+	
+	@Column(length=255)
+	private String avatar;
+	
+	@Column(length=255)
+	private String fbProfile;
+	
+	@Column(length=255)
+	private String twitterProfile;
+	
+	@Column(length=255)
+	private String linkedinProfile;
 
 	public Short getId() {
 		return id;
@@ -53,6 +74,54 @@ public class Admin {
 
 	public void setLastLogin(Date lastLogin) {
 		this.lastLogin = lastLogin;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<BlogPost> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<BlogPost> posts) {
+		this.posts = posts;
+	}
+
+	public String getAvatar() {
+		return avatar;
+	}
+
+	public void setAvatar(String avatar) {
+		this.avatar = avatar;
+	}
+
+	public String getFbProfile() {
+		return fbProfile;
+	}
+
+	public void setFbProfile(String fbProfile) {
+		this.fbProfile = fbProfile;
+	}
+
+	public String getTwitterProfile() {
+		return twitterProfile;
+	}
+
+	public void setTwitterProfile(String twitterProfile) {
+		this.twitterProfile = twitterProfile;
+	}
+
+	public String getLinkedinProfile() {
+		return linkedinProfile;
+	}
+
+	public void setLinkedinProfile(String linkedinProfile) {
+		this.linkedinProfile = linkedinProfile;
 	}
 
 }
