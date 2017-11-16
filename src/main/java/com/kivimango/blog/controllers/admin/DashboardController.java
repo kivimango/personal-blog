85002ee8f63c30e99022eb2ab3d8a16e3cf4a732
kivimango <balazsviktor23@gmail.com>
2017-11-16 11:19:58 +0100

    Added hide post feature and desc ordering to pageable

--- src/main/java/com/kivimango/blog/controllers/admin/DashboardController.java
+++ src/main/java/com/kivimango/blog/controllers/admin/DashboardController.java
@@ -3,6 +3,8 @@ package com.kivimango.blog.controllers.admin;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
@@ -50,7 +52,7 @@ public class DashboardController {
 	
 	@GetMapping("/dashboard/posts")
 	public String listPosts(@ModelAttribute("message") String flashMessage, Model model, 
-			Pageable pageable, RedirectAttributes attr, @AuthenticationPrincipal AdminDetail currentAdmin) {
+			@PageableDefault(value=25, page=0, sort="uploaded", direction=Direction.DESC) Pageable pageable, RedirectAttributes attr, @AuthenticationPrincipal AdminDetail currentAdmin) {
 		if(flashMessage != null) {
 			model.addAttribute("flashMessage", flashMessage);
 		}
@@ -95,6 +97,13 @@ public class DashboardController {
 		}
 		blogposts.edit(slug, form);
 		redAttrs.addFlashAttribute("message", "Blogbejegyzés sikeresen szerkesztve !");
+		return REDIRECT_TO_DASHBOARD_POSTS;
+	}
+	
+	@PostMapping("/dashboard/posts/hide/{slug}")
+	public String hidePost(@PathVariable String slug, RedirectAttributes redAttrs) throws BlogPostNotFoundException {
+		blogposts.hideOrPublish(slug);
+		redAttrs.addFlashAttribute("message", "Blogbejegyzés láthatósága megváltoztatva!");
 		return REDIRECT_TO_DASHBOARD_POSTS;
 	}
 
