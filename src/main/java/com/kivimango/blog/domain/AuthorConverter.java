ea8dc755cbfd8f4e9b76b9b26404126da5c6c8d4
kivimango <balazsviktor23@gmail.com>
2017-11-23 19:00:55 +0100

    Using costructor insted of setters

--- src/main/java/com/kivimango/blog/domain/AuthorConverter.java
+++ src/main/java/com/kivimango/blog/domain/AuthorConverter.java
@@ -5,12 +5,7 @@ import com.kivimango.blog.domain.entity.Admin;
 public class AuthorConverter {
 	
 	public AuthorView convert(final Admin author) {
-		AuthorView converted = new AuthorView();
-		converted.setName(author.getName());
-		converted.setAvatar(author.getAvatar());
-		converted.setFbProfile(author.getFbProfile());
-		converted.setTwitterProfile(author.getTwitterProfile());
-		converted.setLinkedinProfile(author.getLinkedinProfile());
-		return converted;
+		return new AuthorView(author.getName(), author.getAvatar(),
+				author.getFbProfile(), author.getTwitterProfile(), author.getLinkedinProfile());
 	}
 }
