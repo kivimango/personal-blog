505b7323e5ad6f6507b6aef01b5f135a8514436b
kivimango <balazsviktor23@gmail.com>
2017-12-16 14:16:54 +0100

    Added findByTags()

--- src/main/java/com/kivimango/blog/repositories/BlogPostRepository.java
+++ src/main/java/com/kivimango/blog/repositories/BlogPostRepository.java
@@ -1,10 +1,12 @@
 package com.kivimango.blog.repositories;
 
+import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 import com.kivimango.blog.domain.entity.BlogPost;
+import com.kivimango.blog.domain.entity.Tag;
 
 /**
  * @author kivimango
@@ -17,5 +19,6 @@ public interface BlogPostRepository extends PagingAndSortingRepository<BlogPost,
 	
 	Page<BlogPost> findAllByHidden(Pageable pageable, Boolean status);
 	BlogPost getPostBySlug(String slug);
+	List<BlogPost> findByTags(Tag tag);
 
 }
