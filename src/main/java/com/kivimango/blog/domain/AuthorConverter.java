3d76c96fd3a9a9dc9ce8fda035a1f5372a5ae359
kivimango <balazsviktor23@gmail.com>
2018-01-20 20:35:39 +0100

    Overloaded convert() method with list param

--- src/main/java/com/kivimango/blog/domain/AuthorConverter.java
+++ src/main/java/com/kivimango/blog/domain/AuthorConverter.java
@@ -1,5 +1,7 @@
 package com.kivimango.blog.domain;
 
+import java.util.ArrayList;
+import java.util.List;
 import com.kivimango.blog.domain.entity.Admin;
 
 public class AuthorConverter {
@@ -7,5 +9,13 @@ public class AuthorConverter {
 	public AuthorView convert(final Admin author) {
 		return new AuthorView(author.getName(), author.getAvatar(),
 				author.getFbProfile(), author.getTwitterProfile(), author.getLinkedinProfile());
+	}
+	
+	public List<AuthorView> convert(final List<Admin> authors) {
+		List<AuthorView> converted = new ArrayList<>();
+		for(int i=0; i<authors.size(); i++) {
+			converted.add(convert(authors.get(i)));
+		}
+		return converted;
 	}
 }
