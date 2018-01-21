72635e944f249b019965e1e5aca44d71051bbed6
kivimango <balazsviktor23@gmail.com>
2018-01-21 20:36:49 +0100

    Added default admin, fix login

--- src/main/java/com/kivimango/blog/config/WebSecurityConfig.java
+++ src/main/java/com/kivimango/blog/config/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.kivimango.blog.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -8,6 +9,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -25,20 +27,26 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private UserDetailsService service;
 	
+	@Value("${security.user.name}")
+	private String root;
+	
+	@Value("${security.user.password}")
+	private String pw;
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		// Don't change the strength parameter in the encoder's constructor, otherwise it will break the login
 		auth.userDetailsService(service).passwordEncoder(new BCryptPasswordEncoder(12));
+		// adding a default admin - values comes from the cmdline parameters
+		auth.inMemoryAuthentication().withUser(root).password(pw).authorities(new SimpleGrantedAuthority("ROLE_ADMIN"));
 	}
 
 	@Profile("dev")
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		//http.csrf().disable();
-		//http.headers().frameOptions().disable();
 		http
 			.authorizeRequests()
-				.antMatchers("/dashboard/**").hasRole("ADMIN").anyRequest().permitAll()
+				.antMatchers("/dashboard/**").hasRole("ADMIN")
 				.antMatchers("/resources/**").permitAll()
 				.antMatchers("/**").permitAll()
 				.and()
@@ -49,7 +57,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.permitAll()
 			.and()
 				.logout()
-				.logoutUrl("dashboard/kilep")
+				.logoutUrl("/dashboard/logout")
 				.logoutSuccessUrl("/dashboard/login?logout")
 				.permitAll();
 	}
