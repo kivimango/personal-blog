316373bf8e8d6c7b415541ea7e8137ca334fc9fc
kivimango <balazsviktor23@gmail.com>
2018-01-22 10:14:21 +0100

    Added application.properties to pass tests

--- src/test/java/com/kivimango/blog/PersonalBlogApplicationTests.java
+++ src/test/java/com/kivimango/blog/PersonalBlogApplicationTests.java
@@ -2,12 +2,19 @@ package com.kivimango.blog;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PersonalBlogApplicationTests {
+	
+	@Value("${security.user.name}")
+	private String username;
+	
+	@Value("${security.user.password}")
+	private String password;
 
 	@Test
 	public void contextLoads() {
