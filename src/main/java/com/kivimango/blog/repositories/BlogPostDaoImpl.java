c6d9fcf7450a735bf436d95d44045613a70ed504
kivimango <sapka@raklap-PC>
2018-02-18 18:42:56 +0100

    Changed queries to match postgresql syntax

--- src/main/java/com/kivimango/blog/repositories/BlogPostDaoImpl.java
+++ src/main/java/com/kivimango/blog/repositories/BlogPostDaoImpl.java
@@ -33,16 +33,14 @@ public class BlogPostDaoImpl implements BlogPostRepository {
 		this.jdbc = jdbc;
 		this.author = author;
 	}
-
-	// TODO: implement exist checking before insert
 	
 	@Override
 	public List<BlogPost> findAll() {
 		List<BlogPost> posts = new ArrayList<BlogPost>();
-		String query = "SELECT blogpost_id, content, slug, title, edited, uploaded, hidden, GROUP_CONCAT("+Schema.TAGS_TABLE+".tag) as tags FROM " + Schema.POSTS_TABLE
+		String query = "SELECT blogpost_id, content, slug, title, edited, uploaded, hidden, string_agg("+Schema.TAGS_TABLE+".tag, ',') as tags FROM " + Schema.POSTS_TABLE
 				+ " LEFT JOIN " + Schema.POST_TAGS_LOOKUP_TABLE + " ON "+Schema.POST_TAGS_LOOKUP_TABLE+".blog_post_id = " +Schema.POSTS_TABLE+".blogpost_id"
 				+ " LEFT JOIN " + Schema.TAGS_TABLE+" ON "+Schema.POST_TAGS_LOOKUP_TABLE+".tags_id = " +Schema.TAGS_TABLE+".t_id"
-				+ " GROUP BY content, uploaded ORDER BY uploaded DESC;";
+				+ " GROUP BY content, uploaded, blogpost_id, slug, title, edited, hidden ORDER BY uploaded DESC;";
 		posts = jdbc.query(query, new BlogPostRowMapper(author));
 		return posts;
 	}
@@ -59,60 +57,55 @@ public class BlogPostDaoImpl implements BlogPostRepository {
 			return new Page<BlogPost>(pageNum, postCount, posts);
 		}
 		
-		String query = "SELECT blogpost_id, SUBSTRING(content, 0, 100) as content, slug, title, edited, uploaded, hidden, GROUP_CONCAT("+Schema.TAGS_TABLE+".tag) as tags FROM " + Schema.POSTS_TABLE
+		String query = "SELECT blogpost_id, SUBSTRING(content, 0, 100) as content, slug, title, edited, uploaded, hidden, string_agg("+Schema.TAGS_TABLE+".tag, ',') as tags FROM " + Schema.POSTS_TABLE
 				+ " LEFT JOIN " + Schema.POST_TAGS_LOOKUP_TABLE + " ON "+Schema.POST_TAGS_LOOKUP_TABLE+".blog_post_id = " +Schema.POSTS_TABLE+".blogpost_id"
 				+ " LEFT JOIN " + Schema.TAGS_TABLE+" ON "+Schema.POST_TAGS_LOOKUP_TABLE+".tags_id = " +Schema.TAGS_TABLE+".t_id"
-				+ " WHERE hidden = 0"
-				+ " GROUP BY content, uploaded ORDER BY uploaded DESC"
+				+ " WHERE hidden = false"
+				+ " GROUP BY content, uploaded, blogpost_id, slug, title, edited, hidden ORDER BY uploaded DESC"
 				+ " LIMIT ? OFFSET ?;";
 		posts = jdbc.query(query, new Object[] {pageSize, (pageNum-1) * pageSize}, new BlogPostRowMapper(author));
-		
 		return new Page<BlogPost>(pageNum, postCount, posts);
 	}
 
 	@Override
 	public BlogPost getPostBySlug(String slug) {
-		String query = "SELECT blogpost_id, content, slug, title, edited, uploaded, hidden, GROUP_CONCAT("+Schema.TAGS_TABLE+".tag) as tags FROM " + Schema.POSTS_TABLE 
+		String query = "SELECT blogpost_id, content, slug, title, edited, uploaded, hidden, string_agg("+Schema.TAGS_TABLE+".tag, ',') as tags FROM " + Schema.POSTS_TABLE 
 				+ " LEFT JOIN " + Schema.POST_TAGS_LOOKUP_TABLE + " ON "+Schema.POSTS_TABLE+".blogpost_id = "+Schema.POST_TAGS_LOOKUP_TABLE+".blog_post_id" 				
 				+ " LEFT JOIN "+Schema.TAGS_TABLE+" ON "+Schema.TAGS_TABLE+".t_id = "+Schema.POST_TAGS_LOOKUP_TABLE+".tags_id"
-				+ " WHERE "+Schema.POSTS_TABLE+".slug = ? AND hidden = 0 GROUP BY content, uploaded LIMIT 1;";
+				+ " WHERE "+Schema.POSTS_TABLE+".slug = ? AND hidden = false GROUP BY content, uploaded, blogpost_id, slug, title, edited, uploaded, hidden LIMIT 1;";
 		BlogPost post = null;
 		try {
 			post =  jdbc.queryForObject(query, new Object[]{slug}, new BlogPostRowMapper(author));
 		} catch (EmptyResultDataAccessException ignore) {}
 		return post;
 	}
-
-	// TODO: tags not shown up in the results
 	
 	@Override
 	public List<BlogPost> findByTags(String tag) {
 		List<BlogPost> posts = new ArrayList<BlogPost>();
 		String query = "SELECT blogpost_id, content, edited, slug, title, uploaded, hidden, tags FROM "+Schema.POSTS_TABLE+" P"
 				+ " INNER JOIN " 
-				+ "(SELECT "+Schema.POST_TAGS_LOOKUP_TABLE+".BLOG_POST_ID, tag, GROUP_CONCAT(tag) as tags FROM "+Schema.TAGS_TABLE
+				+ "(SELECT "+Schema.POST_TAGS_LOOKUP_TABLE+".BLOG_POST_ID, tag, string_agg(tag, ',') as tags FROM "+Schema.TAGS_TABLE
 				+ " INNER JOIN "+Schema.POST_TAGS_LOOKUP_TABLE+" ON "+Schema.TAGS_TABLE+".T_ID = "+Schema.POST_TAGS_LOOKUP_TABLE+".TAGS_ID AND tag = ?"
-				+ " GROUP BY BLOG_POST_ID ) T ON T.BLOG_POST_ID = P.BLOGPOST_ID AND hidden = 0;";
+				+ " GROUP BY BLOG_POST_ID, "+Schema.TAGS_TABLE+".tag ) T ON T.BLOG_POST_ID = P.BLOGPOST_ID AND hidden = false;";
 		posts = jdbc.query(query, new Object[]{tag}, new BlogPostRowMapper(author));
 		return posts;
 	}
 	
-	// TODO: tags not shown up in the results
-	
 	@Override
 	public List<BlogPost> findByTitleOrSlugOrContentOrTags(String query) {
 		String like = "%" + query + "%";
-		String sql = "SELECT blogpost_id, content, slug, title, edited, uploaded, hidden, GROUP_CONCAT("+Schema.TAGS_TABLE+".tag) as tags FROM " + Schema.POSTS_TABLE
+		String sql = "SELECT blogpost_id, content, slug, title, edited, uploaded, hidden, string_agg("+Schema.TAGS_TABLE+".tag, ',') as tags FROM " + Schema.POSTS_TABLE
 				+ " LEFT JOIN " + Schema.POST_TAGS_LOOKUP_TABLE + " ON "+Schema.POST_TAGS_LOOKUP_TABLE+".blog_post_id = " +Schema.POSTS_TABLE+".blogpost_id"
 				+ " LEFT JOIN " + Schema.TAGS_TABLE+" ON "+Schema.POST_TAGS_LOOKUP_TABLE+".tags_id = " +Schema.TAGS_TABLE+".t_id"
-				+ " WHERE (content LIKE ? OR slug LIKE ? OR title LIKE ?) AND hidden = 0"
-				+ " GROUP BY content, uploaded ORDER BY uploaded DESC;";
+				+ " WHERE (content LIKE ? OR slug LIKE ? OR title LIKE ?) AND hidden = false"
+				+ " GROUP BY content, uploaded, blogpost_id, slug, title, edited, hidden ORDER BY uploaded DESC;";
 		return jdbc.query(sql, new Object[] {like, like, like}, new BlogPostRowMapper(author));
 	}
 
 	@Override
 	public List<BlogPost> findFirst5ByOrderByUploadedDesc() {
-		String query = "SELECT title, slug FROM " + Schema.POSTS_TABLE + " WHERE hidden = 0 ORDER BY uploaded DESC LIMIT 5;";
+		String query = "SELECT title, slug FROM " + Schema.POSTS_TABLE + " WHERE hidden = false ORDER BY uploaded DESC LIMIT 5;";
 		return jdbc.query(query, (rs, i) -> {
 			BlogPost post = new BlogPost();
 			post.setAuthor(author);
