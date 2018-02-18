c6e18223dc0e49c50b569bcdf20fda9cdc579b2b
kivimango <sapka@raklap-PC>
2018-02-18 12:39:34 +0100

    Made truly immutable

--- src/main/java/com/kivimango/blog/domain/Author.java
+++ src/main/java/com/kivimango/blog/domain/Author.java
@@ -38,32 +38,16 @@ public class Author {
 		return facebook;
 	}
 
-	public void setFacebook(String facebook) {
-		this.facebook = facebook;
-	}
-
 	public String getTwitter() {
 		return twitter;
-	}
-
-	public void setTwitter(String twitter) {
-		this.twitter = twitter;
 	}
 
 	public String getLinkedin() {
 		return linkedin;
 	}
 
-	public void setLinkedin(String linkedin) {
-		this.linkedin = linkedin;
-	}
-
 	public String getGithub() {
 		return github;
-	}
-
-	public void setGithub(String github) {
-		this.github = github;
 	}
 
 }
